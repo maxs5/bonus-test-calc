3b5681c926838eb3328834b945754f9fcf0aab77
maxs5 <93215847+maxs5@users.noreply.github.com>
2022-01-23 15:24:18 +0300

    init

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -4,12 +4,10 @@ public class SQRService {
     public int square (int lowerSquare, int upperSquare) {
         int count = 0;
         for (int i = 10; i <= 99; i++) {
-            if (i * i >= lowerSquare && i * i <= upperSquare ) {
+            if (i * i >= lowerSquare && i * i <= upperSquare) {
                 count = count + 1;
-
-                }
             }
-            return count;
         }
+        return count;
     }
-
+}
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -4,33 +4,15 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class SQRServiceTest {
+class SQRServiceTestsquare {
 
     @Test
-    public void testLowerAndUpperSquare() {
-        SQRService service = new SQRService();
-        int count = 0;
-        int lowerSquare = 200;
-        int upperSquare = 500;
-        int resultSQRService = 19;
-        for (int i = 10; i <= 99; i++) {
-            if (i * i >= lowerSquare && i * i <= upperSquare) {
-                count = count + 1;
-                long actual = service.square(lowerSquare, upperSquare);
-                assertEquals(resultSQRService, actual);
-            }
-        }
-
-    }
-}
-
-    @Test
-    public void testwhenSquareRectangle() {
+    void testDefaultLowerAndUpperSquare() {
         SQRService service = new SQRService();
         int count = 0;
         int lowerSquare = 100;
-        int upperSquare = 900;
-        int resultSQRService = 29;
+        int upperSquare = 1000;
+        int resultSQRService = 22;
         for (int i = 10; i <= 99; i++) {
             if (i * i >= lowerSquare && i * i <= upperSquare) {
                 count = count + 1;
@@ -38,6 +20,21 @@ public class SQRServiceTest {
                 assertEquals(resultSQRService, actual);
             }
         }
+    }
 
+    @Test
+    void testWhenSquareBig() {
+        SQRService service = new SQRService();
+        int count = 0;
+        int lowerSquare = 100;
+        int upperSquare = 10001;
+        int resultSQRService = 90;
+        for (int i = 10; i <= 99; i++) {
+            if (i * i >= lowerSquare&& i * i <= upperSquare) {
+                count = count + 1;
+                long actual = service.square(lowerSquare, upperSquare);
+                assertEquals(resultSQRService, actual);
+            }
+        }
     }
 }
